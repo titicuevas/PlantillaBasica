8cf96f771cfc0e4e716d7afda796af3c93192a86
titicuevas <enrique.cuevas@iesdonana.org>
2022-02-24 18:10:13 +0100

    Preparando en clase

---
+++ database/migrations/2022_02_24_164615_create_lineas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lineas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained('pedidos');
+            $table->foreignId('articulo_id')->constrained('articulos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lineas');
+    }
+};
